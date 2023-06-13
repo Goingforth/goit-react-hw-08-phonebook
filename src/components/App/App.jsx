df806de0230db234ffd87c342a57d14375a618c7
Oleksandr Siukov <as-service2008@ukr.net>
2023-06-13 22:01:27 +0300

    +

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,3 +1,10 @@
-export const App = () => {
-  return <div></div>;
+import { Routes, Route } from 'react-router-dom';
+const App = () => {
+  return (
+    <div>
+      <Routes></Routes>
+      {/* <ToastContainer autoClose={3000} theme={'colored'} /> */}
+    </div>
+  );
 };
+export default App;
