817a69498f1d409d8b62d935b53df439b651353a
Oleksandr Siukov <as-service2008@ukr.net>
2023-06-20 09:02:15 +0300

    +

---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,24 @@
+import RegisterForm from 'components/RegisterForm/RegisterForm';
+const Register = () => {
+  return (
+    <div>
+      <title>Registration</title>
+      <RegisterForm />
+    </div>
+  );
+};
+export default Register;
+
+// import { Helmet } from 'react-helmet';
+// import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+
+// export default function Register() {
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Registration</title>
+//       </Helmet>
+//       <RegisterForm />
+//     </div>
+//   );
+// }
