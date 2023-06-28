e2edcad416c4c237ee9c0d2004dc821ffa7ce37d
Oleksandr Siukov <as-service2008@ukr.net>
2023-06-29 00:51:38 +0300

    +

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -3,21 +3,46 @@ import { deleteContact } from 'redux/contacts/operations';
 
 import DeleteIcon from '@mui/icons-material/Delete';
 import Typography from '@mui/material/Typography';
+import { Box } from '@mui/material';
 
 import css from './Contactitem.module.css';
 const ContactItem = ({ id, name, number }) => {
   const dispatch = useDispatch();
   return (
     <li className={css.item}>
-      <Typography>{name}:</Typography>
-      <Typography>{number}</Typography>
-
-      <DeleteIcon
-        onClick={() => {
-          dispatch(deleteContact(id));
+      <Box>
+        <Typography>{name}:</Typography>
+      </Box>
+      <Box
+        sx={{
+          display: {
+            xs: 'flex',
+            alignItems: 'center',
+            gap: 80,
+            justifyContent: 'space-between',
+          },
         }}
-      />
+      >
+        <Typography>{number}</Typography>
+        <DeleteIcon
+          onClick={() => {
+            dispatch(deleteContact(id));
+          }}
+        />
+      </Box>
     </li>
   );
 };
 export default ContactItem;
+{
+  /* <li className={css.item}>
+  <Typography>{name}:</Typography>
+  <Typography>{number}</Typography>
+
+  <DeleteIcon
+    onClick={() => {
+      dispatch(deleteContact(id));
+    }}
+  />
+</li>; */
+}
